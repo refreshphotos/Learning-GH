8ba08b8a3622535852f738830f6c72a1ff54f297
Matt <info@refreshphotos.com.asu>
2022-08-01 09:54:38 +1000

    Initial Commit

---
+++ Shared/gitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubApp.swift
+//  Shared
+//
+//  Created by Matt on 1/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct gitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
